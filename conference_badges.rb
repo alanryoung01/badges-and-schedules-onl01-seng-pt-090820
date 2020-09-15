6d38d1f78f1e1acd9a6192149c884d587a1ec2bd
Alan Young <alanryoung01@gmail.com>
2020-09-15 08:44:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,8 @@
 def badge_maker(name)
 return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  people = []
+  attendees.each do |name|
+    people.push("Hello, my name is #{name}.")
